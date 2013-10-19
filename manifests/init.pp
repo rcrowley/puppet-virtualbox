92c17c73fda4e0b695db8899ad806db4f56aa7b3
Richard Crowley <r@rcrowley.org>
2013-10-19 12:35:22 -0700

    Preliminary Arch Linux support.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,17 @@
 class virtualbox($version = "4.3.0", $build = "89960", $prefix = "/usr/local") {
 
-    Exec {
-        require => File["$prefix/lib/vbox-tftpboot/ubuntu-installer/amd64", "$prefix/lib/vbox-tftpboot/ubuntu-installer/amd64/boot-screens"],
-    }
     exec {
-        "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/ubuntu-installer/amd64/initrd.gz http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/boot-screens/vesamenu.c32":;
-        "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/ubuntu-installer/amd64/initrd.gz http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz":;
-        "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/ubuntu-installer/amd64/linux http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux":;
-        "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/ubuntu-installer/amd64/linux http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/pxelinux.0":;
+        ["/usr/bin/curl -o $prefix/lib/vbox-tftpboot/arch/archiso.img http://ftp.osuosl.org/pub/archlinux/iso/latest/arch/boot/x86_64/archiso.img",
+         "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/arch/pxelinux.0 https://releng.archlinux.org/pxeboot/boot/pxelinux.0",
+         "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/arch/vmlinuz http://ftp.osuosl.org/pub/archlinux/iso/latest/arch/boot/x86_64/vmlinuz",
+         "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/arch/vesamenu.c32 https://releng.archlinux.org/pxeboot/boot/vesamenu.c32"]:
+            require => File["$prefix/lib/vbox-tftpboot/arch"];
+        "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/ubuntu/ubuntu-installer/amd64/boot-screens/vesamenu.c32 http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/boot-screens/vesamenu.c32":
+            require => File["$prefix/lib/vbox-tftpboot/ubuntu/ubuntu-installer/amd64/boot-screens"];
+        ["/usr/bin/curl -o $prefix/lib/vbox-tftpboot/ubuntu/ubuntu-installer/amd64/initrd.gz http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz",
+        "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/ubuntu/ubuntu-installer/amd64/linux http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux",
+        "/usr/bin/curl -o $prefix/lib/vbox-tftpboot/ubuntu/ubuntu-installer/amd64/pxelinux.0 http://archive.ubuntu.com/ubuntu/dists/precise-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/pxelinux.0"]:
+            require => File["$prefix/lib/vbox-tftpboot/ubuntu/ubuntu-installer/amd64"];
     }
 
     File {
@@ -63,17 +67,31 @@ class virtualbox($version = "4.3.0", $build = "89960", $prefix = "/usr/local") {
             mode => 0644,
             source => "puppet:///modules/virtualbox/lib/vbox-preseed.rb";
         ["$prefix/lib/vbox-tftpboot",
-         "$prefix/lib/vbox-tftpboot/pxelinux.cfg",
-         "$prefix/lib/vbox-tftpboot/ubuntu-installer",
-         "$prefix/lib/vbox-tftpboot/ubuntu-installer/amd64",
-         "$prefix/lib/vbox-tftpboot/ubuntu-installer/amd64/boot-screens"]:
+         "$prefix/lib/vbox-tftpboot/arch",
+         "$prefix/lib/vbox-tftpboot/arch/pxelinux.cfg",
+         "$prefix/lib/vbox-tftpboot/ubuntu",
+         "$prefix/lib/vbox-tftpboot/ubuntu/pxelinux.cfg",
+         "$prefix/lib/vbox-tftpboot/ubuntu/ubuntu-installer",
+         "$prefix/lib/vbox-tftpboot/ubuntu/ubuntu-installer/amd64",
+         "$prefix/lib/vbox-tftpboot/ubuntu/ubuntu-installer/amd64/boot-screens"]:
             ensure => directory,
             mode => 0755;
-        "$prefix/lib/vbox-tftpboot/pxelinux.0":
+        "$prefix/lib/vbox-tftpboot/arch/pxelinux.cfg/default":
+            content => template("virtualbox/arch-pxelinux.cfg-default.erb"),
+            mode => 0644;
+
+        # Clean up the old layout.
+        ["$prefix/lib/vbox-tftpboot/pxelinux.0",
+         "$prefix/lib/vbox-tftpboot/ubuntu-installer"]:
+            ensure => absent,
+            force => true,
+            recurse => true;
+
+        "$prefix/lib/vbox-tftpboot/ubuntu/pxelinux.0":
             ensure => link,
             target => "ubuntu-installer/amd64/pxelinux.0";
-        "$prefix/lib/vbox-tftpboot/pxelinux.cfg/default":
-            content => template("virtualbox/pxelinux.cfg-default.erb"),
+        "$prefix/lib/vbox-tftpboot/ubuntu/pxelinux.cfg/default":
+            content => template("virtualbox/ubuntu-pxelinux.cfg-default.erb"),
             mode => 0644;
     }
 
